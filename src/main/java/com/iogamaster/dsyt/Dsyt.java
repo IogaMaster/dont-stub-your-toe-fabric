e9b0ad885354cfc14bf4328c7c839e353b1a7e25
IogaMaster <iogamastercode@gmail.com>
2022-06-29 19:09:43 -0600

    feat: :fire: Set up mod metadata

---
+++ src/main/java/com/iogamaster/dsyt/Dsyt.java
@@ -0,0 +1,12 @@
+package com.iogamaster.dsyt;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Dsyt implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        
+    }
+    
+}

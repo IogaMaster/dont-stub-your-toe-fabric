c57964eba2d9e7b099d8f88b8962eb6822966813
IogaMaster <iogamastercode@gmail.com>
2022-06-30 12:40:35 -0600

    feat: :fire: 1.18

--- src/main/java/com/iogamaster/dsyt/Dsyt.java
+++ src/main/java/com/iogamaster/dsyt/Dsyt.java
@@ -1,32 +1,34 @@
 package com.iogamaster.dsyt;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 
-public class Dsyt implements ModInitializer {
+public class Dsyt implements ClientModInitializer {
     private static MinecraftClient client = MinecraftClient.getInstance();
     private static boolean changed = false;
 
-    @Override
-    public void onInitialize() {
-        onTick();
-    }
 
+    @Override
+    public void onInitializeClient() {
+        onTick();
+        
+    }
+    
     private void onTick() {
         ClientTickEvents.START_CLIENT_TICK.register(listener -> {
-            if(client.world != null)
-			if(!changed) {
-				if(!client.options.getAutoJump().getValue()) { 
-                    if(client.player.isSprinting()) {
-                        changed = true;
-                        client.options.getAutoJump().setValue(true);
+                if(client.world != null)
+                if(!changed) {
+                    if(!client.options.getAutoJump().getValue()) { 
+                        if(client.player.isSprinting()) {
+                            changed = true;
+                            client.options.getAutoJump().setValue(true);
+                        }
                     }
+                } else if(!client.player.isSprinting()) {
+                    client.options.getAutoJump().setValue(false);
+                    changed = false;
                 }
-            } else if(!client.player.isSprinting()) {
-                client.options.getAutoJump().setValue(false);
-                changed = false;
-            }
         });
     }
 }

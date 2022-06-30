994a7f70b5fa88df79d0ee550fab4e7b7df49b3f
IogaMaster <iogamastercode@gmail.com>
2022-06-29 21:49:24 -0600

    feat: :sparkles: Add autojump when sprinting

--- src/main/java/com/iogamaster/dsyt/Dsyt.java
+++ src/main/java/com/iogamaster/dsyt/Dsyt.java
@@ -1,12 +1,33 @@
 package com.iogamaster.dsyt;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 
 public class Dsyt implements ModInitializer {
 
+    private static boolean changed = false;
+
     @Override
     public void onInitialize() {
-        
+
+
+        ClientTickEvents.START_CLIENT_TICK.register(client -> {
+            if(MinecraftClient.getInstance().world != null)
+			if(!changed) {
+				if(!MinecraftClient.getInstance().options.getAutoJump().getValue()) { 
+                    if(MinecraftClient.getInstance().player.isSprinting()) {
+                        changed = true;
+                        MinecraftClient.getInstance().options.getAutoJump().setValue(true);
+                    }
+                }
+            } else if(!MinecraftClient.getInstance().player.isSprinting()) {
+                MinecraftClient.getInstance().options.getAutoJump().setValue(false);
+                changed = false;
+            }
+        });
     }
-    
 }
